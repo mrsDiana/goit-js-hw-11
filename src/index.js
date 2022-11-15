5185dd2e2c85a1524e205ae314d007085db60ccf
Diana Bukovska <dianavladimirovna1409@gmail.com>
2022-11-15 18:07:54 +0200

    async function

--- src/index.js
+++ src/index.js
@@ -14,70 +14,28 @@ function onLoadMore() {
   newApiService.fetchImages().then(resultOfSerch).catch(onFetchError);
 }
 
-function searchImages(e) {
+async function searchImages(e) {
   e.preventDefault();
   resetResult();
   const {
     elements: { searchQuery },
   } = e.currentTarget;
+  if (!searchQuery.value) {
+    resetResult();
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
   newApiService.page = 1;
   newApiService.query = searchQuery.value;
-  newApiService.fetchImages().then(resultOfSerch).catch(onFetchError);
-}
-
-function hiddenBtn() {
-  loadMoreBtn.classList.remove('hidden');
+  const fetchImg = await newApiService.fetchImages();
+  const promise = await 
+  Notiflix.Notify.info(
+    `Hooray! We found ${newApiService.totalHits} images.`
+  );
 }
 
 function resetResult() {
   gallery.innerHTML = '';
-}
-
-function resultOfSerch(data) {
-  const images = data.hits;
-  if (images.length === 0) {
-    throw new Error();
-  } else if (images.length >= newApiService.perPage) {
-    mapSerchResult(images);
-    hiddenBtn();
-  } else {
-    mapSerchResult(images);
-    Notiflix.Notify.info(
-      "We're sorry, but you've reached the end of search results."
-    );
-  }
-}
-
-function mapSerchResult(data) {
-  const images = data
-    .map(
-      ({ webformatURL, tags, likes, views, comments, downloads }) =>
-        `<img src="${webformatURL}" alt="${tags}" loading="lazy" />
-<div class="info">
-  <p class="info-item">
-    <b>Likes</b>
-    ${likes}
-  </p>
-  <p class="info-item">
-    <b>Views</b>
-    ${views}
-  </p>
-  <p class="info-item">
-    <b>Comments</b>
-    ${comments}
-  </p>
-  <p class="info-item">
-    <b>Downloads</b>
-    ${downloads}
-  </p>
-</div>`
-    )
-    .join('');
-  gallery.insertAdjacentHTML('beforeend', images);
-}
-
-function onFetchError(error) {
-  Notiflix.Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.'
-  );
 }
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,23 +1,85 @@
+import Notiflix from 'notiflix';
+const axios = require('axios').default;
 export default class NewApiService {
   constructor() {
+    this.totalHits = 0;
     this.serchQuery = '';
     this.page = 1;
     this.perPage = 40;
+    this.gallery = document.querySelector('.gallery');
+    this.loadMoreBtn = document.querySelector('.load-more');
   }
-
-  fetchImages() {
+   async fetchImages() {
     const BASE_URL = 'https://pixabay.com';
     const KEY = '31283318-f84bd36e26b769e2b71141abe';
-    return fetch(
-      `${BASE_URL}/api/?key=${KEY}&q=${this.serchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.perPage}`
-    ).then(response => {
-      if (!response.ok) {
-        throw new Error();
-      }
+    try {
+      const response = await axios.get(
+        `${BASE_URL}/api/?key=${KEY}&q=${this.serchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.perPage}`
+      );
+      const data = await this.resultOfSerch(response.data);
       this.page += 1;
-      return response.json();
-    });
+      return data;
+    } catch (error) {
+    console.log(error);
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+    }
+  };
+
+ resultOfSerch(data) {
+  this.totalHits = data.totalHits;
+  const images=data.hits;
+    if (images.length === 0) {
+      throw new Error();
+    } else if (images.length >= this.perPage) {
+      this.mapSerchResult(images);
+      this.removeHiddenBtn();
+    } else {
+      this.mapSerchResult(images);
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+      this.addHiddenBtn();
+    }
   }
+
+  mapSerchResult(data) {
+    const images = data
+      .map(
+        ({ webformatURL, tags, likes, views, comments, downloads }) =>
+          `<img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${downloads}
+    </p>
+  </div>`
+      )
+      .join('');
+    this.gallery.insertAdjacentHTML('beforeend', images);
+  }
+
+  removeHiddenBtn() {
+    this.loadMoreBtn.classList.remove('hidden');
+  }
+  
+  addHiddenBtn() {
+    this.loadMoreBtn.classList.add('hidden');
+  }
+
   get query() {
     return this.serchQuery;
   }
